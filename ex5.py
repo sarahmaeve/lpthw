272edbe3d3fe00aed71928ad47dd0677d88f94e3
Sarah Murphy <sarahmaeve@users.noreply.github.com>
2017-07-19 20:50:41 -0700

    Exercises 4 and 5 of Learn Python the Hard Way

---
+++ ex5.py
@@ -0,0 +1,21 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+height_in_cm  = round(height * 2.54)
+weight_in_kg = round(weight / 2.2046, 1)
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches or about {height_in_cm} cm tall.")
+print(f"He's {weight} pounds or {weight_in_kg} kilos heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height} and {weight} I get {total}.")
