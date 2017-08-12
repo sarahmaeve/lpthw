bbec526146419515a36fcc40ff5e01aa8d943512
Sarah Murphy <sarahmaeve@users.noreply.github.com>
2017-08-12 16:23:53 -0700

    snippets and ex43

--- snippets1.py
+++ snippets1.py
@@ -20,3 +20,29 @@ prices = [1.25, 1.79, 2.20, 3.30]
 # toss in some interesting formatting while I'm here
 price_list = {k.title(): "${:,.2f}".format(v) for k,v in zip(fruit, prices)}
 print(price_list)
+
+# reverse a list (idiomatic) through slices
+# with a stride of -1
+a = ['b', 23, 'hamhock', -19, 'camel']
+a = a[::-1]
+
+# copy a list through slice notation
+# this is faster than list comprehensions or copy()
+b = a # b copies the reference to the same list
+c = a[:] # c is now a copy
+a.append('coracle')
+b.append('barbell')
+print(f"a:{a}\nb: {b}\nc: {c}")
+
+# https://www.youtube.com/watch?v=OSGv2VnC0go
+# use reversed()
+colors = ['red', 'green', 'blue', 'yellow']
+for color in reversed(colors):
+    print(color)
+
+# need index?
+# "Whenever you're manipulating indices directly, you're probably doing it wrong."
+for i, color in enumerate(colors):
+    print(i, '--->', color)
+
+# use .join rather than + for assembling a list of strings
