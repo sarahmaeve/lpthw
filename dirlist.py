962569022cd54df379f88b4685f948d1a5744d7d
Sarah Murphy <sarahmaeve@users.noreply.github.com>
2018-03-22 17:57:18 -0700

    os.path recursion example

---
+++ dirlist.py
@@ -0,0 +1,14 @@
+"""
+todo: more research on os.path
+"""
+
+def print_directory_contents(sPath):
+    import os
+    for sChild in os.listdir(sPath):
+        sChildPath = os.path.join(sPath,sChild)
+        if os.path.isdir(sChildPath):
+            print_directory_contents(sChildPath)
+        else:
+            print(sChildPath)
+
+print_directory_contents('.')
